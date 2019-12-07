49f4b874ff58d644d3ca399b685c39855b3dc3be
Shmelkow <sol2390@gmail.com>
2019-12-07 18:06:17 +0200

    add js

--- app.js
+++ app.js
@@ -16,3 +16,11 @@ let baBenefitTitleRed = document.querySelectorAll(".ba-benefit__title");
 for (let i = 0; i < 3; i++) {
     baBenefitTitleRed[i].style.color = "red";
 }
+
+//.ba-team .ba-team-list>.ba-col-3>.ba-team-member>.ba-team-member__img
+
+let baTeamMemberImg = document.querySelectorAll(".ba-team-member__img");
+for (let i = 0; i < baTeamMemberImg.length; i++) {
+    console.log(baTeamMemberImg.length);
+    baTeamMemberImg[i].src = "https://i.dailymail.co.uk/i/pix/2017/04/24/10/3189D6E000000578-4439416-image-a-22_1493026439493.jpg";
+}
