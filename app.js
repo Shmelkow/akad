3c3d363bd7b2fb536e6b8aaaa4462018f73de087
Shmelkow <sol2390@gmail.com>
2019-12-07 16:52:41 +0200

    add color

--- app.js
+++ app.js
@@ -7,3 +7,12 @@ baBreadCrumbs.append(breadCrumpsItem.cloneNode(true));
 let addBreadCrumps = document.querySelector(".ba-breadcrumbs__item:last-child a");
 
 addBreadCrumps.innerText = "company";
+
+
+//.ba-section-header + .ba-benefits-list>ba-col-4>.ba-benefit>.ba-benefit__title
+
+let baBenefitTitleRed = document.querySelectorAll(".ba-benefit__title");
+
+for (let i = 0; i < 3; i++) {
+    baBenefitTitleRed[i].style.color = "red";
+}
